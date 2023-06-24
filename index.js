651d0838e6419a09238eff59fdd781105e6aba5c
noranorab <noranorab7@gmail.com>
2023-06-24 18:10:20 +0100

    full repo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const path = require('path');
 
 var app = express();
 
@@ -10,7 +12,22 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+const upload = multer({ dest: 'Bureau/' });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  if (req.file) {
+     const nameWithExtension = req.file.originalname;
+    const extension = path.extname(nameWithExtension);
+    const name = path.basename(nameWithExtension, extension);
+    res.json({
+      name: name,
+      type: req.file.mimetype,
+      size: req.file.size
+    });
+  } else {
+    res.status(400).json({ error: 'No file uploaded' });
+  }
+})
 
 
 
